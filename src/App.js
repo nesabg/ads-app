fc5fff8a276c2a55c0e6e4a791f1395ffe982b5d
nesabg <nesabg2@gmail.com>
2020-07-24 16:52:14 +0300

    initial app commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Comp from './comp';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Test</h1>
+        <Comp title="Never used components" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/comp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Comp = (props) => {
+    return (
+        <h4>{props.title}</h4>
+    )
+}
+
+export default Comp
